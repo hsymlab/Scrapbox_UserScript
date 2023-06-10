416936a01370be51bf83aeb3c4599118d9aea5e0
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2023-06-10 13:51:41 +0900

    add: M2ゼミのページ生成スクリプト update: 欠席・遠隔参加・早退項目の追加 update: 不要箇所の削除＆コードの整理

--- script.js
+++ script.js
@@ -1,45 +1,72 @@
 //Global変数宣言 ---------------------------------------------------------------------->
 
  //論文ゼミ
- //TODO 年度ごとに修正（url, info）
+ // TODO 年度・学期ごとに修正（url, next_role_table）
 const paper = {
-    doc:    "`[Title URL]`->これでURLを添付できる\n" + "[発表スライドをアップするGoogleDrive ",
-    url:    "https://drive.google.com/drive/folders/1J88G1_RtQPMsm0t0oWfodScm_8cTNlZ2]\n",
-    vote:   "[投票画面 https://hsymlab.github.io/votesys/index.html]\n\n",
-    link:   "[]\n\n",
-    result: "[*** --結果--]\n\n\n",
-    // next:   "[** 〜次回の担当〜]\n" + "全体FG（買い出し係）：\n" + "希望お菓子：\n" + "英語論文：\n",
-    next:   "[** 〜次回の担当〜]\n" + "全体FG：\n" + "英語論文：\n",
-    info:   "[https://scrapbox.io/files/64743def089b43001b52ed78.png]\n\n",
-    tag:    "#論文ゼミ"
+    vote:               "[投票画面 https://hsymlab.github.io/votesys/index.html]\n\n",
+    result:             "[*** --結果--]\n\n\n",
+    // next:            "[** 〜次回の担当〜]\n" + "全体FG（買い出し係）：\n" + "希望お菓子：\n" + "英語論文：\n",
+    next:               "[** 〜次回の担当〜]\n" + "全体FG：\n" + "英語論文：\n",
+    next_role_table:    "[https://scrapbox.io/files/64743def089b43001b52ed78.png]\n\n",
+    tag:                "#論文ゼミ"
 }
 
 //進捗ゼミ
-//TODO　年度ごとに修正(url, info)
+// TODO　年度・学期ごとに修正(url, next_role_table)
 const progress = {
-    doc:    "[進捗資料をアップするGoogleDrive ",
-    url:    "https://drive.google.com/drive/folders/1T0XMcyBZSgA9xW8Lj63Rj39c7G23xiFv]\n",
-    vote:   "\n",
-    link:   "",
-    result: "",
-    next:   "[** 〜次回の担当〜]\n" + "全体FG：\n",
-    // + "ブログ執筆者：\n" + "査読者:\n",
-    // info:   "[研究室ブログ執筆 https://scrapbox.io/hsym2022/%E7%A0%94%E7%A9%B6%E5%AE%A4%E3%83%96%E3%83%AD%E3%82%B0%E5%9F%B7%E7%AD%86]\n" + "[https://scrapbox.io/files/633bc6392bdd29001df90e22.png]\n\n",
-    info:   "[https://scrapbox.io/files/64743def089b43001b52ed78.png]\n\n",
-    tag:    "#進捗ゼミ"
+    vote:               "\n",
+    result:             "",
+    next:               "[** 〜次回の担当〜]\n" + "全体FG：\n",
+    // next_role_table:   "[研究室ブログ執筆 https://scrapbox.io/hsym2022/%E7%A0%94%E7%A9%B6%E5%AE%A4%E3%83%96%E3%83%AD%E3%82%B0%E5%9F%B7%E7%AD%86]\n" + "[https://scrapbox.io/files/633bc6392bdd29001df90e22.png]\n\n",
+    next_role_table:    "[https://scrapbox.io/files/64743def089b43001b52ed78.png]\n\n",
+    tag:                "#進捗ゼミ"
 }
+
+
+// メンバーリスト(2023)
+// TODO グループ決めて、各メンバー書き換える。いないところは"--"
+const paper_member = [
+    ["小川","出口", "東川","武田", "吉岡"],
+    ["小原","笹川","名執", "細川", "--"],
+    ["ギータ","林", "新妻","柳谷", "--"]
+];
+
+const progress_member = [
+    ["小川","出口", "東川","武田", "吉岡"],
+    ["小原","笹川","名執", "細川", "--"],
+    ["ギータ","林", "新妻","柳谷", "--"]
+];
+
+// M2
+const m2_member = ['小川', '笹川', '林'];
+
+
 
 
 //ページ作成 ---------------------------------------------------------------------->
 
-//順番のためのルーレットjs
-const shuffle = (member, number, seminar) => {
+// ページ内テキスト生成
+const gen_page = (member, number, seminar) => {
 
-    const { doc, url, vote, link, result, next, info, tag } = seminar;
+    const { result, next, next_role_table, tag } = seminar;
 
-    // 資料・使用するツールのリンクなど
-    // let text = `${doc}${url}${vote}[** 全体FG：]\n\n`;
+    // 全体FG
     let text = '[** 全体FG：]\n\n';
+
+    // 欠席・遠隔参加者
+    text += '[* 欠席：]\n[* 遠隔参加：]\n[* 早退（h:min）：]\n\n\n'
+
+    text += shuffle_members(member, number);
+
+    text += `${result}${next}${next_role_table}[*** --連絡--]\n\n\n${tag}`;
+
+    return text;
+}
+
+
+// P/FGの組み合わせと順番をランダムに決定
+const shuffle_members = (member, number) => {
+    let text = ""
 
     for(let i = member.length -1 ; i > -1 ; i-- ){
         //Fisher–Yatesアルゴリズムでシャッフル(既存のものは偏りがあるらしい)
@@ -53,46 +80,14 @@ const shuffle = (member, number, seminar) => {
     for(let i = 0; i < member[0].length; i++)  {
         //PかFGかの分岐
         text += `[** ${member[number][i]}(FG：${member[number<2 ? number+1: 0][i]})]\n`
-        text += `${link}[**_ Q&A]\n\n[**_ Comment]\n\n\n`;
+
+        text += `[**_ Q&A]\n\n[**_ Comment]\n\n\n`;
     };
-    // if (seminar == progress) {
-        //text += `[** 今週のブログ担当者：(査読者：)]\n`;
-        // text += `[PullRequest https://github.com/hsymlab/LabWeb/pulls]\n[**_ Q&A]\n\n[**_ Comment]\n\n\n`
-    // }
-
-    // 結果・次回の担当・連絡・タグ
-    // text += `${result}${next}${info}[*** --連絡--]\n\n\n${tag}`;
-    text += `${result}${next}${info}[*** --連絡--]\n\n\n${tag}`;
-
-    return text;
-}
-
-function shuffle_text_seminar(member){
-    let pgList = [];
-    while(member.length >= 1){
-           let r = Math.floor(Math.random() * (member.length));
-        pgList.push(member[r]);
-        member = member.filter(n => n !== member[r]);
-    }
-
-    let text = 'FG: '+pgList[0]+'\n'+'FG: '+pgList[1]+'\n'+'FG: '+pgList[2]+'\n'+'FG: 	'+pgList[3]+'\n'+'FG: '+pgList[4]+'\n';
 
     return text
 }
 
-//メンバーリスト(2023)
-// TODO グループ決めて、各メンバー書き換える。いないところは"--"
-const paper_member = [
-    ["小川","出口", "東川","武田", "吉岡"],
-    ["小原","笹川","名執", "細川", "--"],
-    ["ギータ","林", "新妻","柳谷", "--"]
-];
 
-const progress_member = [
-    ["小川","出口", "東川","武田", "吉岡"],
-    ["小原","笹川","名執", "細川", "--"],
-    ["ギータ","林", "新妻","柳谷", "--"]
- ];
 
 
 //論文ゼミ用ボタン ---------------------------------------------------------------------->
@@ -100,19 +95,19 @@ const progress_member = [
 //ボタンを押した際の処理(論文ゼミ/'GroupA'の)
 scrapbox.PopupMenu.addButton({
     title: '論文A',
-    onClick: () => {return shuffle(paper_member, 0, paper);}
+    onClick: () => {return gen_page(paper_member, 0, paper);}
 });
 
 //ボタンを押した際の処理(論文ゼミ/'GroupB'の)
 scrapbox.PopupMenu.addButton({
     title: '論文B',
-    onClick: () => {return shuffle(paper_member, 1, paper);}
+    onClick: () => {return gen_page(paper_member, 1, paper);}
 });
 
 //ボタンを押した際の処理(論文ゼミ/'GroupC'の)
 scrapbox.PopupMenu.addButton({
     title: '論文C',
-    onClick: () => {return shuffle(paper_member, 2, paper);}
+    onClick: () => {return gen_page(paper_member, 2, paper);}
 });
 
 
@@ -121,20 +116,22 @@ scrapbox.PopupMenu.addButton({
 //ボタンを押した際の処理(進捗ゼミ/'GroupA'の)
 scrapbox.PopupMenu.addButton({
     title: '進捗A',
-    onClick: () => {return shuffle(progress_member, 0, progress);}
+    onClick: () => {return gen_page(progress_member, 0, progress);}
 });
 
 //ボタンを押した際の処理(進捗ゼミ/'GroupB'の)
 scrapbox.PopupMenu.addButton({
     title: '進捗B',
-    onClick: () => {return shuffle(progress_member, 1, progress);}
+    onClick: () => {return gen_page(progress_member, 1, progress);}
 });
 
 //ボタンを押した際の処理(進捗ゼミ/'GroupC'の)
 scrapbox.PopupMenu.addButton({
     title: '進捗C',
-    onClick: () => {return shuffle(progress_member, 2, progress);}
+    onClick: () => {return gen_page(progress_member, 2, progress);}
 });
+
+
 
 
 //Pゼミ ---------------------------------------------------------------------->
@@ -142,15 +139,17 @@ scrapbox.PopupMenu.addButton({
 //ボタンを押した際の処理('Pゼミ'の)
 scrapbox.PopupMenu.addButton({
     title: 'Pゼミ',
-    onClick: () => {return createNewSeminarPage_P();}
+    onClick: () => {return gen_page_p();}
 });
 
 //新しいPゼミのページを作る
-const createNewSeminarPage_P = () => {
+const gen_page_p = () => {
 
+    // TODO GitのURLは適宜変更
     let text = `[Github(hsymlab-uec) https://github.com/2023-hsymlab]\n\n`;
 
     // 班一覧
+    // TODO Pゼミの班が決まり次第変更
     const group=[
         "01:Virtual Stock Trade Systemを作ろう！",
         // "02:カーナビ（簡易版） iOS/Android を作ろう！",
@@ -160,7 +159,7 @@ const createNewSeminarPage_P = () => {
         // "06:学生部屋在室情報システムを作ろう！",
     ];
 
-    // 各班のDONE・TODO
+    // 各班のDONE・TODOリスト
     group.map((value) => {
         text += `[** ${value}]\n[**_ DONE]\n\n[**_ TODO]\n\n\n`;
     });
@@ -170,3 +169,46 @@ const createNewSeminarPage_P = () => {
 
     return text;
 };
+
+
+//M2ゼミ ---------------------------------------------------------------------->
+
+//ボタンを押した際の処理('M2ゼミ'の)
+scrapbox.PopupMenu.addButton({
+    title: 'M2ゼミ',
+    onClick: () => {return gen_page_m2(m2_member);}
+});
+
+//新しいM2ゼミのページを作る
+const gen_page_m2 = (member) => {
+
+    let text = ""
+
+    // 発表順をランダムに決定
+    const order = shuffle_arr(member)
+
+    for(let i = 0; i < member.length; i++) {
+        text += `[** ${order[i]}]\n[**_ NOTE]\n\n\n`;
+    }
+
+    // 連絡・タグ
+    text += `[*** --連絡--]\n\n\n#M2ゼミ`;
+
+    return text;
+};
+
+
+// 配列の中身をシャッフル
+const shuffle_arr = (arr) => {
+    for(let i = (arr.length - 1); 0 < i; i--){
+
+        // 0〜(i+1)の範囲で値を取得
+        let r = Math.floor(Math.random() * (i + 1));
+
+        // 要素の並び替えを実行
+        let tmp = arr[i];
+        arr[i] = arr[r];
+        arr[r] = tmp;
+    }
+    return arr;
+}
